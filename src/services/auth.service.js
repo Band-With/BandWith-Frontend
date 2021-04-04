a63775182c4265e76399ad9a1e88e1336a4d9bc0
이정재 <2wjdwo97@gmail.com>
2021-04-04 18:28:25 +0900

    Merge Home.vue

---
+++ src/services/auth.service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/auth/';
+
+class AuthService{
+    login(user){
+        return axios
+            .post(API_URL + 'signin', {
+                username: user.username,
+                pwd: user.pwd
+            })
+            .then(reponse => {
+                if (Response.data.accessToken) {
+                    localStorage.setItem('user', JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+    
+    logout(){
+        localStorage.removeItem('user');
+    }
+
+    register(user){
+        return axios.post(API_URL + 'signup', {
+            username: user.username,
+            pwd: user.pwd,
+            name: user.name
+        })
+    }
+}
+
+export default new AuthService();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
-<div class="home">
-  <div style="position: relative; background-color: #81F7F3 g;">
-    <br><br><br><br><br><br>
+  <div class="home">
+    <div class="main-search d-flex align-items-center justify-content-center">
+      <div class="search-input-wrapper d-flex align-items-center position-relative">
+        <input class="search-input" type="text" v-model="param" @keyup.enter="fetchData()"
+        placeholder="You can call test api. Enter any integer number.">
+        <button class="search-button" @click="fetchData()"></button>
+
+        <div class="position-absolute ml-3" style="">
+          result: {{num}}<br/>
+          (input_num * 2 + 2)<br/>
+          {{ test }}<br/>
+          {{ test2 }}
+        </div>
+      </div>
+    </div>
+    <div class="banner d-flex align-items-center justify-content-center position-relative">
+      <div class="position-relative">
+        <div class="position-absolute" style="right: -10px">
+          <h1>This is a test page.</h1>
+        </div>
+        <img alt="Vue logo" src="../assets/images/logo-with-text.png" class="logo">
+      </div>
+    </div>
+    <div>
+      <Login msg=""/>
+    </div>
   </div>
-  <div style="position: relative; left:10px;top:-110px;">
-    <img src="../assets/logo.png" style="float:left"><br/><br/>
-  </div>
-  <div>
-  <Login msg=""/>
-  </div>
-</div>
 </template>
 
 <script>
@@ -18,18 +34,46 @@ import Login from '@/components/Login.vue'
 
 export default {
   name: 'Home',
-    components: {
-      Login
+  components: {
+    Login
+  },
+  data(){
+    return{
+      param: "",
+      num: ""
     }
+  },
+  computed: {
+  },
+  methods: {
+    fetchData() {
+        axios.get('http://3.133.139.224:8080/testAPI', {
+          params: { param: this.param },
+          timeout: 2000  
+        })
+        .then(res => {
+          this.num = res.data;
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    test(){
+      this.$store.dispatch('test');
+    }
+  },
+  created() {
+    this.fetchData();
+    this.test();
   }
-  </script>
+}
+</script>
 
-  <style type="text/css">
-  body {
-  }
-  img {
-    height:90px;
-    border-style: none;
-  }
-  </style>
+<style type="text/css">
+img {
+  height:90px;
+  border-style: none;
+}
+</style>
 
