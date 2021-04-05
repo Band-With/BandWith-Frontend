ff3ef304f59239d6be38a0212d093cd11d925d02
eastcolumn <kimdj1026@naver.com>
2021-04-05 11:09:39 +0900

    김동주's

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,48 @@
 <template>
-<div class="home">
-  <div style="position: relative; background-color: #81F7F3 g;">
-    <br><br><br><br><br><br>
+  <div class="home">
+      <div style="width:100%;overflow:auto; border-bottom:5px solid gold; margin-bottom:5px; display:table;">
+        <div style="position: relative; left:10px; float:left; padding:2px">
+          <img src="../assets/logo.png" style="float:left">
+        </div>
+        <badger-accordion style="position:relative; width:20%; float:right;">
+          <badger-accordion-item>
+            <template slot="header">로그인 부탁드립니다</template>
+            <template slot="content"><div><button>마이페이지</button><br><button>회원정보 수정</button><br><button>로그아웃</button><br></div></template><br>
+          </badger-accordion-item>
+        </badger-accordion>
+        <div style="width:9%;float:right;display:table-cell; vertical-align:middle; padding-top:15px;">
+          밴드 둘러보기
+        </div>
+      </div>
+      <div>
+        <Login msg="" />
+      </div>
   </div>
-  <div style="position: relative; left:10px;top:-110px;">
-    <img src="../assets/logo.png" style="float:left"><br/><br/>
-  </div>
-  <div>
-  <Login msg=""/>
-  </div>
-</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Login from '@/components/Login.vue'
+  // @ is an alias to /src
+  import Login from '@/components/Login.vue'
+  import {
+    BadgerAccordion,
+    BadgerAccordionItem
+  } from 'vue-badger-accordion'
 
-export default {
-  name: 'Home',
+  export default {
+    name: 'Home',
     components: {
-      Login
+      Login,
+      BadgerAccordion,
+      BadgerAccordionItem
     }
   }
-  </script>
+</script>
 
-  <style type="text/css">
-  body {
-  }
+<style type="text/css">
+  body {}
+
   img {
-    height:90px;
+    height: 90px;
     border-style: none;
   }
-  </style>
-
+</style>
