a237f6540fad07d467015a4b04219fb09efc599e
이정재 <2wjdwo97@gmail.com>
2021-04-17 18:20:27 +0900

    Add 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -5,14 +5,20 @@
     padding-left: 30px;
     padding-right: 30px;
     margin: 0 auto 30px;
-    margin-top: 60px;
+    margin-top: 160px;
 }
 </style>
 
 <template>
     <div class="d-flex">
-        <div class="d-flex main">
-            <span>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</span>
+        <div class="d-flex main justify-content-center align-items-center">
+            <div class="d-flex flex-column">
+                <span class="mb-3" style="font-weight: bold; font-size: 34px; color:#444">404 Page Not Found</span>
+                <span style="font-weight: lighter; font-size: 14px; color:#444">
+                    요청하신 페이지를 찾을 수 없습니다. <br/>
+                    올바른 주소가 입력되었는지 확인해주세요.
+                </span>
+            </div>
         </div>
     </div>
 </template>
