8fafa3bb503493f32ec8ed363f1ff8d0037a567b
이정재 <2wjdwo97@gmail.com>
2021-06-01 17:15:45 +0900

    Implement RSA encode when login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -17,17 +17,39 @@ class AuthService{
         })
     }
 
-    login(user){
-        return axios
+    async login(user){
+        const rsa = require('node-bignumber');
+        let modulus, exp
+        let uid, pwd
+        await axios
+        .get(API_URL + 'rsa')
+        .then(response => {
+            if (response.data !== null) {
+                console.log(response.data)
+                modulus = response.data[0]
+                exp = response.data[1]
+            }
+
+
+            //RSA 암호화 생성
+            const pub = new rsa.Key()
+            pub.setPublic(modulus, exp);
+            
+            //사용자 계정정보를 암호화 처리
+            uid = pub.encrypt(user.username);
+            pwd = pub.encrypt(user.password);
+        });
+
+        return await axios
             .post(API_URL + 'signin', {
-                username: user.username,
-                pwd: user.password
+                username: uid,
+                pwd: pwd
             })
             .then(response => {
                 if (response.data.user !== null) {
                     localStorage.setItem('user', JSON.stringify(response.data))
                 }
-                return response.data;
+                return response.data
             });
     }
     
